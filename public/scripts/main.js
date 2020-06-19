426fcac13edec5c3ec0d32a7fd4132d5d1ba1304
posssible99 <juanpsfi3023@gmail.com>
2020-06-19 14:11:03 -0500

    Add animations with gsap and scrollmagic

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,23 @@
 $("#comment-keypress").keydown(function () {
-    var numbCharact = $(this).val().length
-    $("#numbCharac").html(numbCharact)
+	var numbCharact = $(this).val().length
+	$("#numbCharac").html(numbCharact)
 });
+
+gsap.from(".search-section", { duration: 0.9, y: "-200", ease: "bounce.out" })
+
+var t1 = new TimelineMax({ onUpdate: updatePercentage })
+const controller = new ScrollMagic.Controller();
+t1.from(".game-card-anim", 0.5, { opacity: 0, x: -200 })
+const scene = new ScrollMagic.Scene({
+	triggerElement: ".upcoming",
+	triggerHook: "onLeaving",
+	duration: "74%"
+})
+	.setTween(t1)
+	.addTo(controller)
+
+
+function updatePercentage() {
+	t1.progress();
+	console.log(t1.progress())
+}
